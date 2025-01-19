67d4930eedcff4bb1bd4ef735d8298b0fc79d777
German Mas <german.e.mas@gmail.com>
2025-01-19 20:35:03 -0300

    Spell List Component
    Squashed commit of the following:
    
    commit 0026b6f9f19fd1b4430786b5cdff2c33130ccf9a
    Author: German Mas <german.e.mas@gmail.com>
    Date:   Sun Jan 19 20:34:30 2025 -0300
    
        Spell List Component

--- src/app/spells/list/spell-list.component.spec.ts
+++ src/app/spells/list/spell-list.component.spec.ts
@@ -1,23 +1,64 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { of, throwError } from 'rxjs';
 
 import { SpellListComponent } from './spell-list.component';
+import { SpellsService } from '../spells.service';
+import { APIReference } from '../types';
 
 describe('SpellListComponent', () => {
   let component: SpellListComponent;
   let fixture: ComponentFixture<SpellListComponent>;
+  const spellsServiceSpy = jasmine.createSpyObj('SpellsService', ['list']);
+  const snackbarSpy = jasmine.createSpyObj('MatSbackbar', ['open']);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SpellListComponent]
-    })
-    .compileComponents();
+      imports: [SpellListComponent],
+      providers: [
+        {
+          provide: SpellsService,
+          useValue: spellsServiceSpy,
+        },
+        {
+          provide: MatSnackBar,
+          useValue: snackbarSpy,
+        },
+      ],
+    }).compileComponents();
+  });
+
+  it('should create', () => {
+    fixture = TestBed.createComponent(SpellListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
+    expect(component).toBeTruthy();
+  });
+
+  it('should get the list of spells', () => {
+    const response = [
+      { index: 'fireball' },
+      { index: 'magic-missile' },
+    ] as APIReference[];
+    spellsServiceSpy.list.and.returnValue(of(response));
 
     fixture = TestBed.createComponent(SpellListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    expect(spellsServiceSpy.list).toHaveBeenCalled();
+    expect(component.spellList()).toEqual(response);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should show a snackbar if the request failed', () => {
+    spellsServiceSpy.list.and.returnValue(throwError(() => new Error('error')));
+
+    fixture = TestBed.createComponent(SpellListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
+    expect(spellsServiceSpy.list).toHaveBeenCalled();
+    expect(snackbarSpy.open).toHaveBeenCalled();
   });
 });
--- src/app/spells/list/spell-list.component.ts
+++ src/app/spells/list/spell-list.component.ts
@@ -1,11 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { RouterLink } from '@angular/router';
+import { finalize } from 'rxjs';
+
+import { APIReference } from '../types';
+import { SpellsService } from '../spells.service';
 
 @Component({
   selector: 'app-spell-list',
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatProgressSpinnerModule,
+    RouterLink,
+  ],
   templateUrl: './spell-list.component.html',
-  styleUrl: './spell-list.component.scss'
+  styleUrl: './spell-list.component.scss',
 })
 export class SpellListComponent {
+  private readonly spellsService = inject(SpellsService);
+  readonly snackbar = inject(MatSnackBar);
 
+  readonly loading = signal<boolean>(true);
+  readonly spellList = signal<APIReference[]>([]);
+
+  constructor() {
+    this.getSpells();
+  }
+
+  getSpells() {
+    this.loading.set(true);
+
+    this.spellsService
+      .list()
+      .pipe(
+        finalize(() => {
+          this.loading.set(false);
+        }),
+      )
+      .subscribe({
+        next: (spells) => {
+          this.spellList.set(spells);
+        },
+        error: (err) => {
+          this.snackbar.open('Error getting the list of spells');
+        },
+      });
+  }
+
+  addToSpellbook(index: string) {
+    this.snackbar.open('Added to your spellbook');
+  }
 }
